7b2863ce72f475d8b6e11ad8540072d378b47b95
Ly Van Bong <87922239+LyVanBong@users.noreply.github.com>
2022-01-03 00:17:55 +0700

    Khởi tạo dự án mmo

---
+++ Mmo/MobileApp/MobileApp/App.xaml.cs
@@ -0,0 +1,33 @@
+using MobileApp.ViewModels;
+using MobileApp.Views;
+using Prism;
+using Prism.Ioc;
+using Xamarin.Essentials.Implementation;
+using Xamarin.Essentials.Interfaces;
+using Xamarin.Forms;
+
+namespace MobileApp
+{
+    public partial class App
+    {
+        public App(IPlatformInitializer initializer)
+            : base(initializer)
+        {
+        }
+
+        protected override async void OnInitialized()
+        {
+            InitializeComponent();
+
+            await NavigationService.NavigateAsync("NavigationPage/MainPage");
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterSingleton<IAppInfo, AppInfoImplementation>();
+
+            containerRegistry.RegisterForNavigation<NavigationPage>();
+            containerRegistry.RegisterForNavigation<MainPage, MainPageViewModel>();
+        }
+    }
+}
