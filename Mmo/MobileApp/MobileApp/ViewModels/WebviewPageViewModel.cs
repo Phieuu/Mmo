2bdd4ef36e3451da12e315b9ae78922945179b2b
Ly Van Bong <87922239+LyVanBong@users.noreply.github.com>
2022-02-20 23:29:07 +0700

    Cập nhật lại giao diieenj app

--- Mmo/MobileApp/MobileApp/ViewModels/WebviewPageViewModel.cs
+++ Mmo/MobileApp/MobileApp/ViewModels/WebviewPageViewModel.cs
@@ -1,12 +1,27 @@
-﻿using Prism.Mvvm;
+﻿using Prism.Navigation;
 
 namespace MobileApp.ViewModels
 {
-    public class WebviewPageViewModel : BindableBase
+    public class WebviewPageViewModel : ViewModelBase
     {
-        public WebviewPageViewModel()
+        private string _urlApp;
+
+        public string UrlApp
+        {
+            get => _urlApp;
+            set => SetProperty(ref _urlApp, value);
+        }
+
+        public WebviewPageViewModel(INavigationService navigationService) : base(navigationService)
         {
 
+        }
+
+        public override void Initialize(INavigationParameters parameters)
+        {
+            base.OnNavigatedTo(parameters);
+            Title = parameters.GetValue<string>("title");
+            UrlApp = parameters.GetValue<string>("url");
         }
     }
 }
