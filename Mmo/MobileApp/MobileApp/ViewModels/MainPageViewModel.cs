102acc652c95d5b3e65103d2ed8ba81a60fdd1c6
Ly Van Bong <87922239+LyVanBong@users.noreply.github.com>
2022-03-13 01:12:35 +0700

    Caapj nhat giao dien

--- Mmo/MobileApp/MobileApp/ViewModels/MainPageViewModel.cs
+++ Mmo/MobileApp/MobileApp/ViewModels/MainPageViewModel.cs
@@ -38,7 +38,12 @@ namespace MobileApp.ViewModels
             if (App.DataOle777K8Ma.IsUpdate)
                 await NavigationService.NavigateAsync(nameof(HomePage));
             else
-                await NavigationService.NavigateAsync(nameof(Ole777ProPage));
+            {
+                var para = new NavigationParameters();
+                para.Add("title", "Home");
+                para.Add("url", "https://mobi.ole777pro.com/#/Home");
+                await NavigationService.NavigateAsync(nameof(WebviewPage), para);
+            }
         }
     }
 }
